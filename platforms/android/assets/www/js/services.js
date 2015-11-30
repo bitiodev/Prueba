21a0a69d88a10ef488cc565f6dc1534f6e1c49cd
Bitio Dev <info@bitio.com.ar>
2015-11-30 18:23:48 -0300

    Nueva App

---
+++ platforms/android/assets/www/js/services.js
@@ -0,0 +1,69 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function ($http) {
+
+    // Some fake testing data
+    var chats = [{
+        id: 0,
+        name: 'Bitio Sistemas',
+        lastText: 'Un paso al futuro',
+        face: 'http://www.bitio.com.ar/CommingSoon/favicon.png'
+    },
+    {
+        id: 1,
+        name: 'Elias Morelli',
+        lastText: 'Owner',
+        face: 'http://www.bitio.com.ar/CommingSoon/favicon.png'
+    }];
+
+    GetUser = function () {
+        
+        $http({
+            method: 'GET',
+            url: 'http://localhost/WebService/ws.php'
+               
+                
+           
+        }).
+        success(function (data, status, headers, config) {
+            // any required additional processing here 
+            
+            return data;
+        }).
+        catch(function (data, status) {
+            deferred.reject(data, status, headers, config);
+        });
+
+       
+    }
+
+    return {
+        all: function () {
+           
+         
+            // return GetUser();
+
+            return $http.get('http://localhost/WebService/ws.php')
+                      .then(function(result) {
+                          //resolve the promise as the data
+                          chats=result.data;
+                          return result;
+                      });
+        
+        
+            
+          
+        },
+        remove: function (chat) {
+            chats.splice(chats.indexOf(chat), 1);
+        },
+        get: function (chatId) {
+            for (var i = 0; i < chats.length; i++) {
+                if (chats[i].id === parseInt(chatId)) {
+                    return chats[i];
+                }
+            }
+            return null;
+        }
+    };
+});
